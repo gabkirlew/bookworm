ed378019b3880c94f9e9fb44faf247222de66d5f
gabkirlew <gabriellekirlew@gmail.com>
2021-05-09 12:03:58 -0500

    Working on review class

--- server/src/main/java/models/Review.java
+++ server/src/main/java/models/Review.java
@@ -1,4 +1,55 @@
 package models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Review {
+
+    private String reviewText;
+    private String recommendation;
+    private String score;
+    private Integer id;
+
+    public Review(){
+
+    }
+
+    public Review(String reviewText, String recommendation, String score) {
+        super();
+        this.reviewText = reviewText;
+        this.recommendation = recommendation;
+        this.score = score;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public String getRecommendation() {
+        return recommendation;
+    }
+
+    public void setRecommendation(String recommendation) {
+        this.recommendation = recommendation;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
